5066957546038b875ca49ebf5bdc02597c40b1d5
helloworld664 <50002024+helloworld664@users.noreply.github.com>
2021-01-03 01:40:22 +0800

    fourth commit

---
+++ sql/sourcesql.sql
@@ -0,0 +1,15 @@
+source E://大3上//OOAD面向对象分析与设计//Test Data//auth-schema.sql;
+
+source E://大3上//OOAD面向对象分析与设计//Test Data//goods-schema.sql;
+
+source E://大3上//OOAD面向对象分析与设计//Test Data//order-schema.sql;
+
+source E://大3上//OOAD面向对象分析与设计//Test Data//other-schema.sql;
+
+source E://大3上//OOAD面向对象分析与设计//Test Data//other-data.sql;
+
+source E://大3上//OOAD面向对象分析与设计//Test Data//order-data.sql;
+
+source E://大3上//OOAD面向对象分析与设计//Test Data//goods-data.sql;
+
+source E://大3上//OOAD面向对象分析与设计//Test Data//auth-data.sql;
